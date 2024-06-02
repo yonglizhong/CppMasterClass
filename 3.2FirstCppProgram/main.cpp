28cb3fab3c45bb26ef21c14f851f92fed7cd13fc
Li Zhong <lizhong_48@hotmail.com>
2024-06-02 19:13:07 +0200

    Commit before upload to github repo

---
+++ 3.2FirstCppProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+    std::cout << "how are you doing" << std::endl;
+    std::cout << "how are you doing" << std::endl;
+    std::cout << "how are you doing" << std::endl;
+    return 0;
+
+}
