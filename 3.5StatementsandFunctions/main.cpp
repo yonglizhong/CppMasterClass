4e81bf971a783f4da95892cd362ba324e03e79da
Li Zhong <lizhong_48@hotmail.com>
2024-06-05 23:51:38 +0200

    Statements Testing

---
+++ 3.5StatementsandFunctions/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    int first_number {4};
+    int second_number {3};
+
+    std::cout << "First number :" << first_number << std::endl; 
+    std::cout << "Second number :" << second_number << std::endl;
+
+}
